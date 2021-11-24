adca9d451f55fd5a5e50c627616c4f70b6aa5a20
Kai Mareving <karska.joanna@gmail.com>
2021-11-25 00:42:33 +0100

    Material UI part 2

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const MainLayout = ({children}) => (
-  <div>
-    <PageNav />
+  <div className='MainLayout'>
+    <AppBar>
+      <Toolbar>
+        <PageNav />
+      </Toolbar>
+    </AppBar>
     <main>
       {children}
     </main>
--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import {NavLink, Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import styles from './PageNav.module.scss';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faPizzaSlice } from '@fortawesome/free-solid-svg-icons'
-// import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import Button from '@material-ui/core/Button';
 
 const PageNav = () => {
 
@@ -13,25 +11,20 @@ const PageNav = () => {
       <Grid>
         <Row between="md" middle="xs">
           <Col md={3} lg={2}>
-            <Link to='/'>
-              <div className={styles.logo}>
-                {/* <FontAwesomeIcon icon={faPizzaSlice} /> */}
-                <span className={styles.name}>Admin Panel</span>
-              </div>
-            </Link>
+            <Button component={NavLink} className={styles.logo} exact to={`${process.env.PUBLIC_URL}/`}>Admin Panel</Button>
           </Col>
           <Col md={6}>
             <nav>
-              <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</NavLink>
-              <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</NavLink>
-              <NavLink to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</NavLink>
-              <NavLink to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</NavLink>
-              <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</NavLink>
+              <Button component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</Button>
+              <Button component={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Button>
+              <Button component={NavLink} to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Button>
+              <Button component={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</Button>
+              <Button component={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Button>
             </nav>
           </Col>
           <Col md={3} lg={2}>
             <div className={styles.contact}>
-              {/* <FontAwesomeIcon icon={faPhone} /> */}<span>+48 889 900 900</span>
+              +48 889 900 900
             </div>
           </Col>
         </Row>

9a5e80130004c172576d1db601601447426c4bb0
Kai Mareving <karska.joanna@gmail.com>
2021-11-28 17:54:22 +0100

    Views with MUI: Tables [start working]

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,27 +1,90 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './Tables.module.scss';
-import { Link } from 'react-router-dom';
-import PageTitle from '../../common/PageTitle/PageTitle';
 import Hero from '../../layout/Hero/Hero';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+
+const demoContent = [
+  { id: '1', status: 'free', booking: null },
+  { id: '2', status: 'thinking', booking: null },
+  { id: '3', status: 'booked', booking: 124 },
+  { id: '4', status: 'booked', booking: 228 },
+  { id: '5', status: 'booked', booking: 305 },
+  { id: '6', status: 'booked', booking: 411 },
+];
+
+const renderActions = status => {
+  switch (status) {
+    case 'free':
+      return (
+        <>
+          <Button>thinking</Button>
+          <Button>new booking</Button>
+        </>
+      );
+    case 'thinking':
+      return (<Button>new booking</Button>);
+    case 'booked':
+      return (<Button>booked</Button>);
+    default:
+      return null;
+  }
+};
 
 const Tables = (props) => {
   return (
-    <div className={styles.component}>
-      <Hero titleText='Tables' imageSrc='https://i.imgur.com/84kZj2f.jpg' />
-      <PageTitle text={props.title} />
-      <h2>Id: {props.match.params.id}</h2>
-      <ul className={styles.linkList}>
-        <li><Link to={`${process.env.PUBLIC_URL}/tables/booking/54`} activeclassname='active'>Booking
-        </Link></li>
-        <li><Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} activeclassname='active'>New booking
-        </Link></li>
-        <li><Link to={`${process.env.PUBLIC_URL}/tables/events/34`} activeclassname='active'>Events
-        </Link></li>
-        <li><Link to={`${process.env.PUBLIC_URL}/tables/events/new`} activeclassname='active'>New event
-        </Link></li>
-      </ul>
-    </div>
+    <Paper className={styles.component}>
+      <Hero titleText={props.title} imageSrc='https://i.imgur.com/84kZj2f.jpg' />
+      <Button className={styles.button} component={NavLink} to={`${process.env.PUBLIC_URL}/tables/booking/new`} activeClassName='active'>New Table
+      </Button>
+      <Button className={styles.button} component={NavLink} to={`${process.env.PUBLIC_URL}/tables/booking/:id`} activeClassName='active'> Edit Table
+      </Button>
+      <Button className={styles.button} component={NavLink} to={`${process.env.PUBLIC_URL}/tables/events/new`} activeClassName='active'> New Event
+      </Button>
+      <Button className={styles.button} component={NavLink} to={`${process.env.PUBLIC_URL}/tables/events/:id`} activeClassName='active'> Edit Event
+      </Button>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Table</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Booking</TableCell>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {demoContent.map(row => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell>
+                {row.status}
+              </TableCell>
+              <TableCell>
+                {row.booking && (
+                  <Button className={styles.button} component={NavLink} to={`${process.env.PUBLIC_URL}/tables/booking/${row.booking}`}>
+                    {row.booking}
+                  </Button>
+                )}
+              </TableCell>
+              <TableCell>
+                {renderActions(row.status)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+    </ Paper>
   );
 };
 

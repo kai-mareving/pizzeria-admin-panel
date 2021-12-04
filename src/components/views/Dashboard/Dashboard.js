aa2a1d255506a9f6ce55d562103845f83095d7fc
Kai Mareving <karska.joanna@gmail.com>
2021-12-04 02:39:44 +0100

    MUI: Dashboard [done]

--- src/components/views/Dashboard/Dashboard.js
+++ src/components/views/Dashboard/Dashboard.js
@@ -1,34 +1,52 @@
 import React from 'react';
 import styles from './Dashboard.module.scss';
 import PropTypes from 'prop-types';
-import Section from '../../layout/Section/Section';
 import Hero from '../../layout/Hero/Hero';
-import List from '../../common/List/List';
-import ListItem from '../../common/ListItem/ListItem';
-// import { faUtensils } from '@fortawesome/free-solid-svg-icons';
-// import { faPray } from '@fortawesome/free-solid-svg-icons';
-// import { faBreadSlice } from '@fortawesome/free-solid-svg-icons';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import Container from '@material-ui/core/Container';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const orderStatistics = [
+  {
+    id: '1',
+    type: 'REMOTE',
+    quantity: 234,
+  },
+  {
+    id: '2',
+    type: 'LOCAL',
+    quantity: 574,
+  },
+];
 
 const Dashboard = (props) => (
-  <Section variant='has-hero'>
+  <div className={styles.component}>
     <Hero titleText={props.title} imageSrc='https://i.imgur.com/Nnusvb4.jpg' />
-    <Grid>
-      <Row middle="md">
-        <Col md={6}>
-          <h1 className={styles.title}>Welcome to Dashboard</h1>
-          <p className={styles.intro}>On the right you will find links to other available modules.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies eros eu nisl vestibulum iaculis. Maecenas dapibus lorem quis ex ornare dignissim. Donec lobortis commodo augue, vel accumsan nunc facilisis vitae. Suspendisse bibendum congue sapien ut elementum. Nunc pellentesque, tortor et dignissim fermentum, purus mi sagittis neque, ut iaculis arcu lorem nec tellus.</p>
-        </Col>
-        <Col xs={12} md={5} mdOffset={1} xl={3} xlOffset={2}>
-          <List variant='solid'>
-            <ListItem title='Tables View' /* icon={faUtensils} */ />
-            <ListItem title='Waiter View' /* icon={faPray} */ />
-            <ListItem title='Kitchen View' /* icon={faBreadSlice} */ />
-          </List>
-        </Col>
-      </Row>
-    </Grid>
-  </Section>
+    <Container maxWidth='lg' className={styles.container}>
+      <Paper className={styles.intro}>
+        <Table size="large">
+          <TableHead className={styles.tablehead}>
+            <TableRow>
+              <TableCell>Order Type</TableCell>
+              <TableCell>Order Quantity</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orderStatistics.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>{row.type}</TableCell>
+                <TableCell>{row.quantity}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </ Paper>
+    </ Container>
+  </div>
 );
 
 Dashboard.propTypes = {
